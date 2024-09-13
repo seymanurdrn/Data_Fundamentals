e84899a89d47b572aeaf611c1e905f27359a9f53
Şeyma Nur Duran <seymaanurduran@gmail.com>
2024-09-13 13:54:46 +0300

    Create main.py

---
+++ 04_Python/main.py
@@ -0,0 +1,8 @@
+yil = 2024
+dogum_yili = int(input("Doğum yılınızı giriniz: "))
+yas = (yil - dogum_yili)
+print(yas)
+
+isim_listesi = ["Ali", 2005, "Istanbul", 70.5, True]
+print(isim_listesi[2])
+
